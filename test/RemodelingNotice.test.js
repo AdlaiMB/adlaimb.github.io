7c19bd2a8968d87217f0c0d823874a81711eec2c
Adlai M.B <adlaimb@gmail.com>
2025-11-21 22:03:43 -0800

    install vitest package and create test for RemodelingNotice component

---
+++ test/RemodelingNotice.test.js
@@ -0,0 +1,13 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { expect, test } from "vitest";
+import RemodelingNotice from "../src/components/RemodelingNotice.astro";
+
+test("correct render of RemodelingNotice component", async () => {
+  const container = await AstroContainer.create();
+  const result = await container.renderToString(RemodelingNotice);
+
+  expect(result).toContain(
+    'img src="/favicon.svg" width="70" height="70" alt="personal logo"'
+  );
+  expect(result).toContain("Website under remodeling, come back later.");
+});
