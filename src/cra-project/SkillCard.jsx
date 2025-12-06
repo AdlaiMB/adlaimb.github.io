6baba0276e872c64e0c010060a638eb8add2606f
Adlai M.B <adlaimb@gmail.com>
2025-12-06 14:53:11 -0800

    Remove the old website file built with create-react-app

--- src/cra-project/SkillCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function SkillCard({ name, link }) {
-  return (
-    <div className="skill-card">
-      <div className="bg"></div>
-      <img src={`/skills/${link}`} alt={name} width="68" height="68" />
-      <p className="semibold-font-weight">{name}</p>
-    </div>
-  );
-}
