a5e340cd854c2e9bc6c2a38d580d548b5197f71f
Adlai M.B <adlaimb@gmail.com>
2026-07-13 22:51:27 -0700

    update base in the astro config and update file paths for the images

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://adlaimb.github.io",
-  base: "/Personal-Website",
   integrations: [react()],
 });
