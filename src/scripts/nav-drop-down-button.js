3623137cada66cbbc4878c191a7e55f9887c0a23
Adlai M.B <adlaimb@gmail.com>
2026-07-13 17:47:48 -0700

    add code to hide drop down nav when a link is clicked

--- src/scripts/nav-drop-down-button.js
+++ src/scripts/nav-drop-down-button.js
@@ -1,9 +1,26 @@
 const navigationDropDownButton = document.getElementById(
   "webpage-header-navigation-drop-down-button",
 );
+const navigationLinks = document.querySelectorAll(
+  ".webpage-header-navigation-link",
+);
 
 navigationDropDownButton.addEventListener("click", () => {
-  const isExpanded =
+  const isDropDownVisible =
     navigationDropDownButton.getAttribute("aria-expanded") === "true";
-  navigationDropDownButton.setAttribute("aria-expanded", `${!isExpanded}`);
+  navigationDropDownButton.setAttribute(
+    "aria-expanded",
+    `${!isDropDownVisible}`,
+  );
 });
+
+navigationLinks.forEach((navigationLink) =>
+  navigationLink.addEventListener("click", () => {
+    const isDropDownVisible =
+      navigationDropDownButton.getAttribute("aria-expanded") === "true";
+
+    if (isDropDownVisible) {
+      navigationDropDownButton.setAttribute("aria-expanded", "false");
+    }
+  }),
+);
